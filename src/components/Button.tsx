02aa5a5c0db2b17eed6c19381d2392f768cff975
George Mshvidobadze <giorgim@one-v.co.il>
2026-09-04 20:26:27 +0400

    - added primary and secondary buttons

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import type { ReactNode } from "react";
+
+interface ButtonProps {
+    type: 'submit' | 'button' | 'reset';
+    variant: 'primary' | 'secondary';
+    handleClick: () => void;
+    children: ReactNode;
+}
+
+const Button = ({children, type='button', variant, handleClick}: ButtonProps) => {
+    return (
+        <button className={
+            variant==='primary' ? 'w-full bg-emerald-600 py-4 text-lg font-bold text-white transition-colors hover:bg-emerald-700 active:bg-emerald-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600 disabled:opacity-60 disabled:cursor-not-allowed' : 'w-full bg-emerald-600 py-4 text-lg font-bold text-white transition-colors hover:bg-emerald-700 active:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600 disabled:opacity-60 disabled:cursor-not-allowed'
+        } type={type} onClick={handleClick}>
+            {children}
+        </button>
+    )
+}
+
+export default Button;
