4ae66f781d8f1a465c1a89de8d47b3c3d646ef18
nikoloz-code23 <73937823+nikoloz-code23@users.noreply.github.com>
2026-09-10 16:38:08 +0400

    feat: Add props interfaces, fetch request wrapper and auth services

---
+++ src/environments/environments.ts
@@ -0,0 +1,4 @@
+// In the future, make sure this is in a .env file.
+export const ENV = {
+  API_URL : "http://localhost:3000"
+}
---
+++ src/services/app.client.ts
@@ -0,0 +1,23 @@
+import { ENV } from "../environments/environments";
+
+const APP_URL = ENV.API_URL;
+
+export async function request<T>(endpoint : string, options : RequestInit = {}) : Promise<T> {
+  const headers = new Headers(options.headers);
+  // This will make sure that the data we send is a JSON.
+  if (headers.has("Content-Type")) headers.set("Content-Type", "application/json");
+  
+  let data = undefined;
+  try {
+    // credentials: "include" is to include the Cookies.
+    const response = await fetch(`${APP_URL}/${endpoint}`, {...options, headers, credentials: "include"});
+    if (!response.ok) throw new Error("Error with fetching data.");
+
+    data = await response.json();
+  } catch(e) {
+    console.error(e);
+    return data;
+  }
+
+  return data;
+}
