51cb1874cd35ee46919641462a6399fd1acf17bb
George Mshvidobadze <giorgim@one-v.co.il>
2026-09-11 19:38:57 +0400

    - feat: add redux toolkit and configure auth slice

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { User } from "../../models/AuthProps";
+
+interface AuthState {
+    user: User | null;
+    isAuthenticated: boolean;
+    isLoading: boolean;
+}
+
+const initialState: AuthState = {
+    user: null,
+    isAuthenticated: false,
+    isLoading: true
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setCredentials: (state, action: PayloadAction<User>) => {
+            state.user = action.payload;
+            state.isAuthenticated = true;
+            state.isLoading = false;
+        },
+        logoutUser: (state) => {
+            state.user = null;
+            state.isAuthenticated = false;
+            state.isLoading = false;
+        },
+        setLoading: (state, action: PayloadAction<boolean>) => {
+            state.isLoading = action.payload;
+        }
+    }
+})
+
+export const {setCredentials, logoutUser, setLoading} = authSlice.actions;
+export default authSlice.reducer;
